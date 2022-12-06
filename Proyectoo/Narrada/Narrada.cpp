1300da08f552bac806012f8c92f6c580affbf158
Refered <117299993+Refered@users.noreply.github.com>
2022-12-06 08:28:04 -0600

    Final de la historia narrada
    Este es el commit con la historia narrada completa sin errores

--- Proyectoo/Narrada/Narrada.cpp
+++ Proyectoo/Narrada/Narrada.cpp
@@ -12,7 +12,7 @@ int main()
     Sleep(700);
     std::cout << "Buenos dias!!!\n";
     Sleep(700);
-    std::cout<<"Por fin despiertas (dice una voz resuena en todo el cuarto)\n";
+    std::cout << "Por fin despiertas (dice una voz resuena en todo el cuarto)\n";
 
     Sleep(1000);
     std::cout << "Que haras ahora?\nPuedes salir del cuarto(1) o explorar la habitacion(2)\n";
@@ -28,7 +28,7 @@ int main()
     if (decision == 2) {
         std::cout << "-El cuarto parece inmaculado sin mas que la cama y un televisor antiguo sin ninguna conexion a la luz-\nSolo te queda salir de la habitacion\n";
         Sleep(700);
-        std::cout << "-Sales del cuarto y ves dos caminos la izquierda que parece un pasillo mas oscuro y derecha el cual es un pasillo luminoso con blancas paredes- Que camino tomaremos hoy? la izquierda(1) o la derecha(2)\n";
+        std::cout << "-Sales del cuarto y ves dos caminos la izquierda que parece un pasillo mas oscuro y derecha el cual es un pasillo luminoso con blancas paredes-\nQue camino tomaremos hoy? la izquierda(1) o la derecha(2)\n";
         std::cin >> camino;
     }
     Sleep(700);
@@ -52,7 +52,7 @@ int main()
                     std::cout << " - Sales del edificio y te encuentras libre, volteas atras y ves la casa abandonada de un solo piso en la que estabas sin saber como ni por que llegaste ahi - \n"; Sleep(1200);
                     std::cout << "La voz que resuena dice-Wow escapaste, espero disfrutes lo que te depara la vida fuera, si es que acaso lograste salir\n"; Sleep(1200);
                     std::cout << "FIN";
-;               exit(0);
+                    ;               exit(0);
                 }
                 if (decision == 1 && llave == false) {
                     std::cout << "-Parece que la puerta esta cerrada y solo puedes seguir bajando\n"; Sleep(1200);
@@ -60,7 +60,9 @@ int main()
 
                 }
                 Sleep(1200);
-                if (decision == 2) {
+            }
+        }
+        if (decision == 2) {
                     std::cout << "-Sigues bajando hasta que ves otra puerta, esta vez esta media abierta, decides entrar(1) o seguir bajando(2)\n"; Sleep(1000);
                     std::cin >> decision;
                     if (decision == 1) {
@@ -81,20 +83,20 @@ int main()
                     if (decision == 2) {
                         std::cout << "-Sigues bajando y encuentras por fin el final de las escaleras y hay dos puertas, una con seguro(1) y otra sin seguro(2)-\n"; Sleep(1000);
                         std::cin >> decision;
-                        if (decision == 2 || llave==false) {
+                        if (decision == 2 || llave == false) {
                             std::cout << "Al parecer toca abrir la puerta sin seguro-Dijo la voz de las paredes-\n"; Sleep(1000);
                             std::cout << "-Abres la puerta y entras a la habitacion-\n"; Sleep(1000);
                             std::cout << "-Ves una sala de control con una persona sentada frente a varios monitores-\n"; Sleep(1000);
                             std::cout << "-Se voltea el hombre-\n"; Sleep(1000);
                             std::cout << "Veo que me encontraste, deberia premiarte por esto\n"; Sleep(1000);
-                            std::cout<<"- Saca un arma y boom!- \n"; Sleep(1000);
+                            std::cout << "- Saca un arma y boom!- \n"; Sleep(1000);
                             std::cout << "FIN"; Sleep(1000);
                             exit(0);
                         }
                         if (decision == 1 && llave == true) {
 
                             std::cout << "-Parece que  la puerta da hacia un pasillo con una luz al fondo-\n"; Sleep(1000);
-                            std::cout<<"-Caminas hacia la luz y sales al exterior a un vasto campo de flores en el cual por fin eres libre-\n"; Sleep(1000);
+                            std::cout << "-Caminas hacia la luz y sales al exterior a un vasto campo de flores en el cual por fin eres libre-\n"; Sleep(1000);
                             std::cout << "FIN";
                             exit(0);
                         }
@@ -102,8 +104,8 @@ int main()
                     }
                 }
 
-            }
-        }
+            
+        
         break;
 
     case 2:
@@ -131,31 +133,35 @@ int main()
 
                 std::cout << "-Al presionar el boton resuena el edificio pero no pasa mas, por lo que decides seguir subiendo-\n"; Sleep(1000);
                 llave = true;
-                decision += 1;
-
-            }
-            if (decision == 3) {
-
-                if (llave == true) {
-                
-                    std::cout << "-Parece haber una escotilla con unas escaleras de mano, la curiosidad te gana y subes las escaleras llegando a un tejado, ves el hermoso cielo y te recuestas en el piso, simplemente sintiendote libre-\n"; Sleep(1000);
-                    std::cout << "FIN";
-                    exit(0);
-               }
-                std::cout << "-Parece que solo queda una puerta, no te queda mas remedio que entrar-\n"; Sleep(1000);
-                std::cout << "-Entras y de repente caes al vacio-\n"; Sleep(1400);
-                std::cout << "Buenos dias!!!\n";
-                Sleep(700);
-                std::cout << "Por fin despiertas (dice una voz resuena en todo el cuarto)\n";
-                exit(0);
 
             }
 
         }
+        if (decision == 3 || decision == 2) {
+
+            
+            if (llave == true) {
+
+                std::cout << "-Parece haber una escotilla con unas escaleras de mano, la curiosidad te gana y subes las escaleras llegando a un tejado, ves el hermoso cielo y te recuestas en el piso, simplemente sintiendote libre-\n"; Sleep(1400);
+                std::cout << "FIN";
+                exit(0);
+            }
+
+            std::cout << "-Sigues subiendo y parece que solo queda una puerta, no te queda mas remedio que entrar-\n"; Sleep(2400);
+            std::cout << "-Entras y de repente caes al vacio-\n"; Sleep(2400);
+            std::cout << "Buenos dias!!!\n";
+            Sleep(700);
+            std::cout << "Por fin despiertas (dice una voz resuena en todo el cuarto)\n";
+            exit(0);
+
+        }
+
+
         break;
 
     default:
         break;
     }
-
 }
+
+
